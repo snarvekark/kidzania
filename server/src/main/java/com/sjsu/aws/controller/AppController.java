dd0349be3ffbdf610708465bb9f0d7ded6cb670e
Jahnavi Rangu <55324369+jrangu@users.noreply.github.com>
2019-11-18 00:33:43 -0800

    Added service to detect labels(AWS rekognition) in an image and uploads image to S3 bucket(Pictures folder)

--- server/src/main/java/com/sjsu/aws/controller/AppController.java
+++ server/src/main/java/com/sjsu/aws/controller/AppController.java
@@ -1,22 +1,21 @@
 package com.sjsu.aws.controller;
 
 import java.io.File;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.amazonaws.services.polly.model.OutputFormat;
 import com.sjsu.aws.service.AppService;
 import com.sjsu.aws.service.S3BucketService;
-
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping(value = "/api")
@@ -26,6 +25,9 @@ public class AppController {
 	
 	@Autowired
 	S3BucketService s3BucketService;
+	
+	@Autowired
+	AppService appService;
 	
 	@PostMapping("/uploadStory")
 	public void updateObject(@RequestPart(value = "file",required = false) MultipartFile file,
@@ -44,5 +46,14 @@ public class AppController {
 	public void displayObject(@RequestParam(value = "bucketname") String bucketname, @RequestParam(value = "username") String username,
 			@RequestParam(value = "key") String key) throws Exception {
 		this.s3BucketService.readFromS3(bucketname,username, key);
+	}
+	
+		
+	@PostMapping("/uploadImage")
+	public List<String> uploadImageForRecognition(@RequestPart(value = "file",required = false) MultipartFile file
+				) throws Exception { 
+			File imageFile = s3BucketService.convertMultiPartToFile(file);
+			s3BucketService.uploadFileTos3bucket(file.getOriginalFilename(), imageFile, "Pictures");			
+			return appService.detectLabelsInFile(imageFile);			
 	}
 }
