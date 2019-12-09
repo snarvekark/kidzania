1399e237bfb2dceac77f5b360513ba1e6d8c3a4c
snarvekark <mscd.sheetal@gmail.com>
2019-12-09 04:01:07 -0800

    UPdated Teacher Navigation

--- client/src/components/TeacherNav.js
+++ client/src/components/TeacherNav.js
@@ -1,49 +1,25 @@
 import React from 'react';
-import { Redirect, withRouter } from "react-router-dom";
+import { Redirect, withRouter, Link } from "react-router-dom";
 
 class TeacherNav extends React.Component {
-  state = {
-    redirect: false
-  }
-
-  setRedirect = () => {
-    this.setState({
-      redirect: true
-    })
-  }
-  renderRedirect = () => {
-    if (this.state.redirect) {
-      return <Redirect to='/Teacher' />
-    }
-  }
-
-  picture(){
-    window.location = '/PictureStory';
-  }
-
-  library(){
-    window.location = '/Library';
-  }
-
  render() {
     return(
       <div>
-        {this.renderRedirect()}
         <div className="container" style={{marginTop: '30px'}}>
           <div className="row">
             <div className="col-sm-20 sidebar">
               <ul className="nav nav-pills flex-column">
                 <li className="nav-item">
-                  <a id= "1" className="nav-link" href="#" onClick={this.setRedirect}>Create Story Book</a>
+                  <Link id= "1" className="nav-link" to="/Teacher">Create Story Book</Link>
                 </li>
                 <li className="nav-item">
-                  <a id= "2" className="nav-link" href="#" onClick={this.picture}>Create Picture Assignment</a>
+                  <Link id= "2" className="nav-link" to="/PictureStory">Create Picture Assignment</Link>
                 </li>
                 <li className="nav-item">
                   <a className="nav-link" href="#">Assign Homework</a>
                   <ul>
                     <li>
-                      <a id= "3" href="#" onClick={this.library}>Assign Story</a>
+                      <Link id= "3" to="/Library">Assign Story</Link>
                     </li>
                   </ul>
                 </li>
